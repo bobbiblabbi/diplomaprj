ce194fb8f6000eaa70a3c00df7c5ded3ddf126b0
Danil Akimov <bobbiblabbi@hotmail.com>
2018-03-06 20:31:47 +0300

    first commit

--- diploma/views.py
+++ diploma/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
-    context = {'hui':'10'}
+    context = {'suffix':'10'}
     return render(request, 'diploma/home.html', context)
